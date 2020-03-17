2a2f7b711264fa4cefaf7aeff0f799408c0fe7f1
irfan irawan <irfan.irawan@tokodistributor.com>
2020-03-17 21:52:11 +0700

    fix: init

---
+++ features/home/src/main/java/com/irfanirawansukirman/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.irfanirawansukirman.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home_activity)
+    }
+}
