d556bb6a1579556f14354c7c47d77d5774565ef7
irfan irawan <irfan.irawan@tokodistributor.com>
2020-04-09 15:54:05 +0700

    fix: add empty module medsoc fix: add empty module location

---
+++ libraries/medsocauthentication/src/main/java/com/irfanirawansukirman/medsocauthentication/Example2Activity.kt
@@ -0,0 +1,12 @@
+package com.irfanirawansukirman.medsocauthentication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Example2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.example2_activity)
+    }
+}
