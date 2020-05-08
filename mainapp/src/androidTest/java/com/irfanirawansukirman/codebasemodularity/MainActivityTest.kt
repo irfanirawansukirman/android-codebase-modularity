b7b3f432263ae6c8dfd02f8d9e2304f7b5185e57
Irfan Irawan Sukirman <dadang.kotz@gmail.com>
2020-05-08 11:24:53 +0700

    otw: update readme

--- mainapp/src/androidTest/java/com/irfanirawansukirman/codebasemodularity/MainActivityTest.kt
+++ mainapp/src/androidTest/java/com/irfanirawansukirman/codebasemodularity/MainActivityTest.kt
@@ -1,9 +1,6 @@
 package com.irfanirawansukirman.codebasemodularity
 
-import org.junit.runner.RunWith
-import org.junit.runners.JUnit4
-
-@RunWith(JUnit4::class)
+//@RunWith(JUnit4::class)
 class MainActivityTest {
 
 //    @get:Rule // global set for activity test.jks
