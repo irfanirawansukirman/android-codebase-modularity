1a8fe1052089a4b1503c7d1bf0fd213c762d349d
Irfan Irawan Sukirman <dadang.kotz@gmail.com>
2020-05-08 09:54:49 +0700

    otw: remove some test

--- mainapp/src/androidTest/java/com/irfanirawansukirman/codebasemodularity/ExampleInstrumentedTest.kt
+++ mainapp/src/androidTest/java/com/irfanirawansukirman/codebasemodularity/ExampleInstrumentedTest.kt
@@ -1,7 +1,7 @@
 package com.irfanirawansukirman.codebasemodularity
 
 /**
- * Instrumented test, which will execute on an Android device.
+ * Instrumented test.jks, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
@@ -9,7 +9,7 @@ package com.irfanirawansukirman.codebasemodularity
 class ExampleInstrumentedTest {
 //    @Test
 //    fun useAppContext() {
-//        // Context of the app under test.
+//        // Context of the app under test.jks.
 //        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
 //        assertEquals("com.irfanirawansukirman.codebasemodularity", appContext.packageName)
 //    }
--- mainapp/src/androidTest/java/com/irfanirawansukirman/codebasemodularity/MainActivityTest.kt
+++ mainapp/src/androidTest/java/com/irfanirawansukirman/codebasemodularity/MainActivityTest.kt
@@ -1,24 +1,16 @@
 package com.irfanirawansukirman.codebasemodularity
 
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.rule.ActivityTestRule
-import com.irfanirawansukirman.codebasemodularity.presentation.MainActivity
-import org.junit.Rule
-import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 
 @RunWith(JUnit4::class)
 class MainActivityTest {
 
-    @get:Rule // global set for activity test
-    val activityRule = ActivityTestRule(MainActivity::class.java)
+//    @get:Rule // global set for activity test.jks
+//    val activityRule = ActivityTestRule(MainActivity::class.java)
 
-    @Test
-    fun recyclerviewIsDisplayed() {
-        onView(withId(R.id.recyclerMain)).check(matches(isDisplayed()))
-    }
+//    @Test
+//    fun recyclerviewIsDisplayed() {
+//        onView(withId(R.id.recyclerMain)).check(matches(isDisplayed()))
+//    }
 }
